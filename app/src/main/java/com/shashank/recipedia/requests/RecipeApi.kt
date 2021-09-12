11fd7d1464007bbd9473d3212338bac98bff55ce
Shashank Sinha <sinhashashank.98@gmail.com>
2021-09-12 20:20:23 +0530

    Retrofit interface methods added

--- app/src/main/java/com/shashank/recipedia/requests/RecipeApi.kt
+++ app/src/main/java/com/shashank/recipedia/requests/RecipeApi.kt
@@ -1,4 +1,24 @@
 package com.shashank.recipedia.requests
 
+import com.shashank.recipedia.requests.responses.RecipeResponse
+import com.shashank.recipedia.requests.responses.RecipeSearchResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface RecipeApi {
+
+    // Search
+    @GET("api/v2/recipes")
+    fun searchRecipes(
+        @Query("q") query: String,
+        @Query("page") page: String
+    ): Call<RecipeSearchResponse>
+
+
+    // Get recipe request
+    @GET("api/v2/recipes")
+    fun getRecipe(
+        @Query("rId") recipeId: String
+    ): Call<RecipeResponse>
 }
