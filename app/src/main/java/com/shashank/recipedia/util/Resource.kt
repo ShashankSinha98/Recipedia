1b6134b39e60f961ff93197a9b4c509ab6fd1adf
Shashank Sinha <sinhashashank.98@gmail.com>
2021-10-07 00:29:01 +0530

    Network status Resource class

---
+++ app/src/main/java/com/shashank/recipedia/util/Resource.kt
@@ -0,0 +1,21 @@
+package com.shashank.recipedia.util
+
+class Resource<T>(
+    val status: Status,
+    val data: T?,
+    val message: String?
+) {
+
+
+    companion object {
+
+        fun <T> success(data: T): Resource<T> = Resource(Status.SUCCESS, data, null)
+
+        fun <T> error(msg: String, data: T) = Resource(Status.ERROR, data, msg)
+
+        fun <T> loading(data: T) = Resource(Status.LOADING, data, null)
+
+    }
+
+    enum class Status {SUCCESS, ERROR, LOADING}
+}
