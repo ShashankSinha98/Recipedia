a9a26f70987d5949ca16c2cd57ae907757cdac8d
Shashank Sinha <sinhashashank.98@gmail.com>
2021-10-08 01:56:43 +0530

    Network Bound Resource (Part 3 of 3)

--- app/src/main/java/com/shashank/recipedia/util/NetworkBoundResource.kt
+++ app/src/main/java/com/shashank/recipedia/util/NetworkBoundResource.kt
@@ -1,5 +1,6 @@
 package com.shashank.recipedia.util
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
@@ -13,6 +14,9 @@ import com.shashank.recipedia.requests.responses.ApiResponse
 abstract class NetworkBoundResource<CacheObject, RequestObject>(
     private val appExecutors: AppExecutors
 ) {
+    private val TAG = "NetworkBoundResource"
+
+
     private val results: MediatorLiveData<Resource<CacheObject>> = MediatorLiveData()
 
     init {
@@ -27,6 +31,7 @@ abstract class NetworkBoundResource<CacheObject, RequestObject>(
 
             if(shouldFetch(cacheObject)) {
                 // get data from network
+                fetchFromNetwork(dbSource)
             } else {
                 results.addSource(dbSource, Observer { cacheObject ->
                     setValue(Resource.success(cacheObject))
@@ -36,6 +41,95 @@ abstract class NetworkBoundResource<CacheObject, RequestObject>(
         })
     }
 
+
+
+
+    // fetch data from network and insert it into db cache
+    /**
+        1) Observe local db
+        2) if <condition/> query the network
+        3) Stop observing local db
+        4) Insert new data into local db
+        5) begin observing local db again to see refreshed data from network
+     */
+    private fun fetchFromNetwork(dbSource: LiveData<CacheObject>) {
+        Log.d(TAG, "fetchFromNetwork called")
+
+        // update live data for loading status. show cached data meanwhile
+        results.addSource(dbSource, Observer { cacheObject->
+            setValue(Resource.loading(cacheObject))
+        })
+
+        // response from retrofit in form of LiveData (not call)
+        val apiResponse: LiveData<ApiResponse<RequestObject>> = createCall()
+
+        results.addSource(apiResponse, Observer { requestObjectApiResponse ->
+            /**
+             *  3 cases -
+             *  1) ApiSuccessResponse
+             *  2) ApiEmptyResponse
+             *  3) ApiErrorResponse
+             * */
+
+            when(requestObjectApiResponse) {
+
+                is ApiResponse.ApiSuccessResponse -> {
+                    Log.d(TAG, "onChanged: ApiSuccessResponse")
+
+                    appExecutors.diskIO().execute { // save response to local db - need to be done on background thread, so used diskIO
+                        // saveCallResult(processResponse(requestObjectApiResponse))
+                        saveCallResult(requestObjectApiResponse.getBody())
+
+                        // pass result to our single source data i.e., results
+                        appExecutors.mainThread().execute { // used main thread as we need to SET value to our single source data i.e., results
+
+                            // reading latest data from db
+                            results.addSource(loadFromDB(), Observer { cacheObject ->
+                                setValue(Resource.success(cacheObject))
+                            })
+                        }
+                    }
+                }
+
+                is ApiResponse.ApiEmptyResponse -> {
+                    Log.d(TAG, "onChanged: ApiEmptyResponse")
+                    // get data from local and set it to our single source data i.e., results
+                    appExecutors.mainThread().execute { // used main thread as we need to SET value to our single source data i.e., results
+                        // As we didn't update db as response was empty, get old data from db
+                        results.addSource(dbSource, Observer { cacheObject -> // NOTE- my logic: loadFromDB() -> dbSource
+                            setValue(Resource.success(cacheObject))
+                        })
+                    }
+                }
+
+                is ApiResponse.ApiErrorResponse -> {
+                    Log.d(TAG, "onChanged: ApiEmptyResponse")
+
+                    // get data from local and set it to our single source data i.e., results
+                    // Also notify that it was error response
+                    appExecutors.mainThread().execute { // used main thread as we need to SET value to our single source data i.e., results
+                        // As we didn't update db as response has error, get old data from db
+                        results.addSource(dbSource, Observer { cacheObject -> // NOTE- my logic: loadFromDB() -> dbSource
+                            setValue(Resource.error(
+                                requestObjectApiResponse.getErrorMessage(),
+                                cacheObject))
+                        })
+                    }
+                }
+            }
+        })
+    }
+
+
+
+
+    private fun processResponse(response: ApiResponse.ApiSuccessResponse<RequestObject>): RequestObject
+            = response.getBody()
+
+
+
+    // must be performed on Main Thread - for immediate results
+    // postValue - background thread, might take some time (when resource/thread are free, only then it will execute)
     private fun setValue(newValue: Resource<CacheObject>) {
         if(results.value != newValue) {
             results.value = newValue
