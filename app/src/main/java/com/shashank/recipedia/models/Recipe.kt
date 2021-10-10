1a1052523b6bf56438cd48e53a636c0a483dc639
Shashank Sinha <sinhashashank.98@gmail.com>
2021-10-10 13:31:35 +0530

    Room Entities

--- app/src/main/java/com/shashank/recipedia/models/Recipe.kt
+++ app/src/main/java/com/shashank/recipedia/models/Recipe.kt
@@ -1,36 +1,52 @@
 package com.shashank.recipedia.models
 
 import android.os.Parcelable
+import androidx.annotation.ColorInt
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Entity(tableName = "recipes")
 data class Recipe(
 
     @Expose
     @SerializedName("title")
+    @ColumnInfo(name="title")
     var title: String?= null,
 
     @Expose
     @SerializedName("publisher")
+    @ColumnInfo(name="publisher")
     var publisher: String?= null,
 
     @Expose
     @SerializedName("ingredients")
+    @ColumnInfo(name="ingredients")
     var ingredients: List<String>?= null,
 
     @Expose
     @SerializedName("id")
+    @PrimaryKey
+    @NonNull
     var recipeId: String?= null,
 
     @Expose
     @SerializedName("imageUrl")
+    @ColumnInfo(name="imageUrl")
     var imageUrl: String?= null,
 
     @Expose
     @SerializedName("socialUrl")
+    @ColumnInfo(name="socialRank")
     var socialRank: Float?= null,
+
+    @ColumnInfo(name="timestamp")
+    var timestamp: Int
 
 ) : Parcelable {
 
